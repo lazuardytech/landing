41cd1e4bcd1352b732b74c772c86933d26e62aef
Ezra Lazuardy <ezralazuardy@icloud.com>
2025-07-11 09:55:09 +0700

    feat: update

--- components/ui/logo.jsx
+++ components/ui/logo.jsx
@@ -2,16 +2,16 @@ import Plus_Jakarta_Sans from "@/lib/fonts/plus-jakarta-sans";
 import Doto from "@/lib/fonts/doto";
 
 export default function Logo(props) {
-	const { className, withTM } = props;
-	return (
-		<div className={`${Plus_Jakarta_Sans.className}`}>
-			<span className={`${className} text-[1rem]`}>
-				Lazuardy
-				<span className={`${Doto.className} font-medium text-[1.1rem] ms-1`}>
-					Tech
-				</span>
-			</span>
-			{withTM && <span className="ms-1 absolute">™</span>}
-		</div>
-	);
+  const { className, withTM } = props;
+  return (
+    <div className={`${Plus_Jakarta_Sans.className}`}>
+      <span className={`${className} text-lg`}>
+        Lazuardy
+        <span className={`${Doto.className} font-medium text-[1.04rem] ms-1`}>
+          Tech
+        </span>
+      </span>
+      {withTM && <span className="ms-1 absolute">™</span>}
+    </div>
+  );
 }
