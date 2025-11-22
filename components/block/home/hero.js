3d33fd6db2a04e30b1d1e2e50ac062de6578597e
Ezra Wijaya <ezralazuardy@icloud.com>
2025-11-22 15:42:41 +0700

    update

--- components/block/home/hero.js
+++ components/block/home/hero.js
@@ -9,108 +9,109 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Hero() {
-	const [text, setText] = useState("software agency");
-	const phrases = [
-		"",
-		"strategy.",
-		"solutions.",
-		"workflow.",
-		"process.",
-		"automation.",
-		"product.",
-		"brand.",
-		"business.",
-		"agency.",
-		"plan.",
-	];
-	const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
-	const [isDeleting, setIsDeleting] = useState(false);
-	const [typedText, setTypedText] = useState("");
-	const [showCursor, setShowCursor] = useState(true);
-	const [baseText, setBaseText] = useState("");
-	const basePhrase = "because you do deserve better ";
-	const typingDelay = 100;
-	const deletingDelay = 50;
-	const pauseDelay = 2000;
+  const phrases = [
+    "",
+    "strategy.",
+    "solutions.",
+    "workflow.",
+    "process.",
+    "automation.",
+    "product.",
+    "brand.",
+    "business.",
+    "agency.",
+    "plan.",
+  ];
+  const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [typedText, setTypedText] = useState("");
+  const [showCursor, setShowCursor] = useState(true);
+  const [baseText, setBaseText] = useState("");
+  const basePhrase = "because you do deserve better ";
+  const typingDelay = 100;
+  const deletingDelay = 50;
+  const pauseDelay = 2000;
 
-	useEffect(() => {
-		const cursorInterval = setInterval(() => {
-			setShowCursor((prev) => !prev);
-		}, 500);
+  useEffect(() => {
+    const cursorInterval = setInterval(() => {
+      setShowCursor((prev) => !prev);
+    }, 500);
 
-		return () => clearInterval(cursorInterval);
-	}, []);
+    return () => clearInterval(cursorInterval);
+  }, []);
 
-	useEffect(() => {
-		let i = 0;
-		const typeBaseText = () => {
-			if (i <= basePhrase.length) {
-				setBaseText(basePhrase.slice(0, i));
-				i++;
-				setTimeout(typeBaseText, 47); // Changed from typingDelay to 50
-			}
-		};
-		typeBaseText();
-	}, []);
+  useEffect(() => {
+    let i = 0;
+    const typeBaseText = () => {
+      if (i <= basePhrase.length) {
+        setBaseText(basePhrase.slice(0, i));
+        i++;
+        setTimeout(typeBaseText, 47); // Changed from typingDelay to 50
+      }
+    };
+    typeBaseText();
+  }, []);
 
-	useEffect(() => {
-		const currentPhrase = phrases[currentPhraseIndex];
+  useEffect(() => {
+    const currentPhrase = phrases[currentPhraseIndex];
 
-		const timeout = setTimeout(
-			() => {
-				if (!isDeleting) {
-					if (typedText === currentPhrase) {
-						setTimeout(() => setIsDeleting(true), pauseDelay);
-						return;
-					}
-					setTypedText(currentPhrase.substring(0, typedText.length + 1));
-				} else {
-					if (typedText === "") {
-						setIsDeleting(false);
-						setCurrentPhraseIndex((current) => (current + 1) % phrases.length);
-						return;
-					}
-					setTypedText(currentPhrase.substring(0, typedText.length - 1));
-				}
-			},
-			isDeleting ? deletingDelay : typingDelay,
-		);
+    const timeout = setTimeout(
+      () => {
+        if (!isDeleting) {
+          if (typedText === currentPhrase) {
+            setTimeout(() => setIsDeleting(true), pauseDelay);
+            return;
+          }
+          setTypedText(currentPhrase.substring(0, typedText.length + 1));
+        } else {
+          if (typedText === "") {
+            setIsDeleting(false);
+            setCurrentPhraseIndex((current) => (current + 1) % phrases.length);
+            return;
+          }
+          setTypedText(currentPhrase.substring(0, typedText.length - 1));
+        }
+      },
+      isDeleting ? deletingDelay : typingDelay,
+    );
 
-		return () => clearTimeout(timeout);
-	}, [typedText, isDeleting, currentPhraseIndex]);
+    return () => clearTimeout(timeout);
+  }, [typedText, isDeleting, currentPhraseIndex]);
 
-	return (
-		<LayoutLine className="pt-[14vh] md:pt-[22vh] px-8">
-			<Title className="text-3xl md:text-6xl text-justify">
-				{baseText}
-				<span>{typedText}</span>
-				{showCursor ? "|" : ""}
-			</Title>
-			<div className="grid grid-cols-1 md:grid-cols-2 w-full mt-8 md:mt-8">
-				<div className="w-full flex space-x-2 justify-start">
-					<Button size="lg" variant="default" asChild>
-						<Link href="/inquiry">Start a Project</Link>
-					</Button>
-					<Button size="lg" variant="link" asChild>
-						<Link
-							href={process.env.NEXT_PUBLIC_CONSULTATION_BOOKING_LINK}
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							Talk to Us
-						</Link>
-					</Button>
-				</div>
-				<div className="w-full flex justify-end mt-8 md:mt-0">
-					<Paragraph className="text-sm">
-						We&apos;re the most respected tech firm in Central Java that gets
-						your challenges and uses the best tech in industries to help you.
-					</Paragraph>
-				</div>
-			</div>
-			<div className="flex w-full h-full aspect-video mt-10 md:mt-2">
-				<HeroVideo />
-			</div>
-		</LayoutLine>
-	);
+  return (
+    <LayoutLine className="pt-[14vh] md:pt-[22vh] px-8">
+      <div className="block w-full min-h-[6rem] lg:min-h-[8rem]">
+        <Title className="text-3xl md:text-6xl text-justify">
+          {baseText}
+          <span>{typedText}</span>
+          {showCursor ? "|" : ""}
+        </Title>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 w-full mt-2 md:mt-8">
+        <div className="w-full flex space-x-2 justify-start">
+          <Button size="lg" variant="default" asChild>
+            <Link href="/inquiry">Start a Project</Link>
+          </Button>
+          <Button size="lg" variant="link" asChild>
+            <Link
+              href={process.env.NEXT_PUBLIC_CONSULTATION_BOOKING_LINK}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Talk to Us
+            </Link>
+          </Button>
+        </div>
+        <div className="w-full flex justify-end mt-8 md:mt-0">
+          <Paragraph className="text-sm">
+            We&apos;re the most respected tech firm in Central Java that gets
+            your challenges and uses the best tech in industries to help you.
+          </Paragraph>
+        </div>
+      </div>
+      <div className="flex w-full h-full aspect-video mt-10 md:mt-2">
+        <HeroVideo />
+      </div>
+    </LayoutLine>
+  );
 }
